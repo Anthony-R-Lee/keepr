d699ca194dc2bf22de1bb697a297a48fb3d78f46
Anthony Lee <anthonylee5519@gmail.com>
2025-01-24 11:31:55 -0700

    files setup

---
+++ server/Services/KeepsRepository.cs
@@ -0,0 +1,11 @@
+namespace keepr.Services;
+
+public class KeepsService
+{
+
+  public KeepsService(KeepsRepository repository)
+  {
+    _repository = repository;
+  }
+  private readonly KeepsRepository _repository;
+}
