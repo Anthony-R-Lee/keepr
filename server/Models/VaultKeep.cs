29c6d5c60687d91444821d2c47ca4d694f651e74
Anthony Lee <anthonylee5519@gmail.com>
2025-01-24 14:39:29 -0700

    vaultkeep model

---
+++ server/Models/VaultKeep.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace keepr.Models;
+
+public class VaultKeep : RepoItem<int>
+{
+  public int KeepId { get; set; }
+  public int VaultId { get; set; }
+  [MaxLength(255)] public string CreatorId { get; set; }
+}
