5d0894989d2e8160b6f6b58e4a4acaed135cf379
Anthony Lee <anthonylee5519@gmail.com>
2025-01-25 01:36:06 -0700

    template for account and profile page

--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -4,14 +4,30 @@ import { AppState } from '../AppState.js';
 
 const account = computed(() => AppState.account)
 
+const keeps = computed(() => AppState.keeps)
+const vaults = computed(() => AppState.vaults)
+
 </script>
 
 <template>
   <div class="about text-center">
     <div v-if="account">
-      <h1>Welcome {{ account.name }}</h1>
-      <img class="rounded" :src="account.picture" alt="" />
-      <p>{{ account.email }}</p>
+      <div>
+
+        <img class="rounded cover-img" :src="account.coverImg" alt="" />
+        <img class="rounded profile-img" :src="account.picture" alt="" />
+        <h1> {{ account.name }}</h1>
+        <p>{{ vaults.length }} Vaults | {{ keeps.length }} Keeps</p>
+      </div>
+      <div>
+
+        <div class="col-md-7 justify-content-center text-start d-flex">
+          <h1>Vaults</h1>
+        </div>
+        <div class="col-md-7 justify-content-center text-start d-flex">
+          <h1>Keeps</h1>
+        </div>
+      </div>
     </div>
     <div v-else>
       <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
@@ -20,7 +36,13 @@ const account = computed(() => AppState.account)
 </template>
 
 <style scoped lang="scss">
-img {
+.profile-img {
+  max-width: 100px;
+  max-height: 5em;
+  border-radius: 50%;
+}
+
+.cover-img {
   max-width: 100px;
 }
 </style>
--- client/src/pages/ProfilePage.vue
+++ client/src/pages/ProfilePage.vue
@@ -1,11 +1,52 @@
 <script setup>
+import { AppState } from '@/AppState';
+import { computed } from 'vue';
+
+
+
+const account = computed(() => AppState.account)
+
+const keeps = computed(() => AppState.keeps)
+const vaults = computed(() => AppState.vaults)
 
 </script>
 
 
 <template>
-  dfdf
+  <div class="about text-center">
+    <div v-if="account">
+      <div>
+
+        <img class="rounded cover-img" :src="account.coverImg" alt="" />
+        <img class="rounded profile-img" :src="account.picture" alt="" />
+        <h1> {{ account.name }}</h1>
+        <p>{{ vaults.length }} Vaults | {{ keeps.length }} Keeps</p>
+      </div>
+      <div>
+
+        <div class="col-md-7 justify-content-center text-start d-flex">
+          <h1>Vaults</h1>
+        </div>
+        <div class="col-md-7 justify-content-center text-start d-flex">
+          <h1>Keeps</h1>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+    </div>
+  </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile-img {
+  max-width: 100px;
+  max-height: 5em;
+  border-radius: 50%;
+}
+
+.cover-img {
+  max-width: 100px;
+}
+</style>
